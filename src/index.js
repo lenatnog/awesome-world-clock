140efb04cf210afbed44727e47717003ea176e26
lenatnog <helena_1306@hotmail.com>
2024-12-10 12:41:07 -0300

    Added Lisbon to the initial cities

--- src/index.js
+++ src/index.js
@@ -24,6 +24,19 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+
+  //Lisbon
+  let lisbonElement = document.querySelector("#lisbon");
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
+    let lisbonTimeElement = lisbonElement.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
+
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM	Do YYYY");
+    lisbonTimeElement.innerHTML = lisbonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
